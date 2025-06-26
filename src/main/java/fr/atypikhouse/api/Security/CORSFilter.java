82fba0c609defd7c6035a69e4d14ddd35ecd12fb
Mohammed ZEGGAI <59542121+mohammed-zeggai@users.noreply.github.com>
2025-06-26 23:52:26 +0200

    Update CORSFilter.java

--- src/main/java/fr/atypikhouse/api/Security/CORSFilter.java
+++ src/main/java/fr/atypikhouse/api/Security/CORSFilter.java
@@ -1,27 +1,24 @@
-package fr.atypikhouse.api.Security;
-
-import java.io.IOException;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;Add commentMore actions
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.stereotype.Component;
+
 import org.springframework.web.filter.OncePerRequestFilter;
 
-@Component
+
 public class CORSFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain filterChain) throws ServletException, IOException {
         res.addHeader("Access-Control-Allow-Origin", "*");
+
         if (req.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(req.getMethod())) {
             // CORS "pre-flight" request
             res.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-            res.addHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+            res.addHeader("Access-Control-Allow-Headers", "Authorization");
+            res.addHeader("Access-Control-Allow-Headers", "Content-Type");
             res.addHeader("Access-Control-Max-Age", "1");
         }
+
         filterChain.doFilter(req, res);
     }
 }
